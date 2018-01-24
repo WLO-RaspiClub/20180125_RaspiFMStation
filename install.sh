707b8458867ce503d9aefd399161328d4c20e84c
Takeshi Ueno <utaani@ueno.org>
2018-01-25 00:03:07 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo apt install -y git make gcc g++ sox libsox-fmt-mp3
 git clone https://github.com/markondej/fm_transmitter.git
 cd fm_transmitter
 make
-wget https://github.com/WLO-RaspiClub/20180125_RaspiFMStation/raw/master/SwelteringAbandon.wav
+wget -N https://github.com/WLO-RaspiClub/20180125_RaspiFMStation/raw/master/SwelteringAbandon.wav
