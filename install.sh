24d5a98622db61421fcb4f982c9806f3cd2e19de
Takeshi Ueno <utaani@ueno.org>
2018-01-25 00:52:24 +0900

    Open JTalkと関連ファイルのインストールを追加

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /home/pi
 sudo apt install -y git make gcc g++ sox libsox-fmt-mp3
+sudo apt install –y open-jtalk
+sudo apt install –u open-jtalk-mecab-naist-jdic hts-voice-nitech-jp-atr503-m001
 git clone https://github.com/markondej/fm_transmitter.git
 cd fm_transmitter
 make
