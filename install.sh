31c591abac5e8c68be31b28c21644009603157a0
Takeshi Ueno <utaani@ueno.org>
2018-01-25 01:18:18 +0900

    open_jtalk用のシェルを追加

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ cd /home/pi
 sudo apt install -y git make gcc g++ sox libsox-fmt-mp3
 sudo apt install –y open-jtalk
 sudo apt install –u open-jtalk-mecab-naist-jdic hts-voice-nitech-jp-atr503-m001
+wget -N https://github.com/WLO-RaspiClub/20180125_RaspiFMStation/raw/master/jtalk.sh
+chmod +x jtalk.sh
 git clone https://github.com/markondej/fm_transmitter.git
 cd fm_transmitter
 make
